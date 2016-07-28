f22ea228675e70615e92fee3e3415ed62ac6ad84
Renan <contato@renanmarks.net>
2016-07-28 18:41:03 -0400

    Added transformation (camera position changed).

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
 #include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include "context.h"
 
 int main(int argc, char *argv[])
@@ -16,7 +18,27 @@ int main(int argc, char *argv[])
     glBindVertexArray(VertexArrayID);
 
     // Create and compile our GLSL program from the shaders
-    GLuint programID = context.getShadersProgramID("shaders/SimpleVertexShader.shader", "shaders/SimpleFragmentShader.shader");
+    GLuint programID = context.getShadersProgramID("shaders/SimpleTransform.shader", "shaders/SimpleFragmentShader.shader");
+
+    // Get a handle for our "MVP" uniform
+    GLuint matrixID = glGetUniformLocation(programID, "MVP");
+
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(
+                    glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+                    glm::vec3(0,0,0), // and looks at the origin
+                    glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model      = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
 
     const GLfloat g_vertex_buffer_data[] = {
        -1.0f, -1.0f, 0.0f,
@@ -45,6 +67,9 @@ int main(int argc, char *argv[])
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(programID);
+
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        glUniformMatrix4fv(matrixID, 1, GL_FALSE, &MVP[0][0]);
 
         // 1rst attribute buffer : vertices
         glEnableVertexAttribArray(0);
