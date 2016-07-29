17dacb7c8de4e90c9886c4ab408386662096ebca
Renan <contato@renanmarks.net>
2016-07-29 18:32:39 -0400

    Cull faces.

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@ int main(int argc, char *argv[])
     // Accept fragment if it closer to the camera than the former one
     glDepthFunc(GL_LESS);
 
+    // Cull triangles which normal is not towards the camera
+    glEnable(GL_CULL_FACE);
+
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
